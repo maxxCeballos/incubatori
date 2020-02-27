e8cabb7a4e7bdd5712743cc184b74cc09e325ccc
Maxx <maxic2405@gmail.com>
2020-02-26 23:39:26 -0300

    [ADD] repositories added

--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,5 @@
 module.exports = {
     User: require('./user.model'),
     Comment: require('./comment.model'),
-    Idea: require('./idea.model');
+    Idea: require('./idea.model')
 }
--- src/startup/container.js
+++ src/startup/container.js
@@ -17,6 +17,9 @@ const Routes = require('../routes');
 // models
 const { User, Comment, Idea } = require('../models');
 
+// repositories
+const { UserRepository, IdeaRepository, CommentRepository } = require('../repositories');
+
 const container = createContainer();
 
 container.register({
@@ -33,6 +36,10 @@ container.register({
         User: asValue(User),
         Idea: asValue(Idea),
         Comment: asValue(Comment)
+    }).register({
+        UserRepository: asClass(UserRepository).singleton(),
+        IdeaRepository: asClass(IdeaRepository).singleton(),
+        CommentRepository: asClass(CommentRepository).singleton()
     });
 
 module.exports = container;
